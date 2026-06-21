6450b10254be66db8921828f62e4de5c2045e2b5
Vandice Tech <amahlephumla80@gmail.com>
2026-06-21 14:26:54 +0200

    commit

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 const apiKey = "2eac11fc61bd1af73b197650a4aba90e";
 
+let map = L.map("map").setView([-26.2041, 28.0473], 10);
+
+L.titleLayer('https://{s}.title.openstreetmap.org/{z}/{x}/{y}.png',{
+    attribution: '&copy; OpenStreetMap contributors'
+}).addTo(map);
+
+let marker = L.marker([-26.2041, 28.0473]).addTo(map);
+
 document.getElementById("searchBtn").addEventListener("click", getWeather);
+
+document.getElementById("cityInput").addEventListener("keypress",  function(e){
+    if(e.key === "Enter") {
+        getWeather();
+    }
+});
 
 async function getWeather() {
 
@@ -19,13 +33,15 @@ try {
     const res = await fetch(url);
     const data = await res.json();
 
+    console.log(data);
+
 if (data.cod !== 200) {
-    alert(data.message);
+    alert(data.message || "City not found");
     return;
 }
 
     document.getElementById("city").innerText = data.name;
-    document.getElementById("temp").innerText = data.main.temp + "°C";
+    document.getElementById("temp").innerText = Math.round(data.main.temp) + "°C";
     document.getElementById("desc").innerText = data.weather[0].description;
     document.getElementById("humidity").innerText = data.main.humidity + "%";
     document.getAnimations("wind").innerText = data.wind.speed +"km/h";
@@ -35,6 +51,15 @@ if (data.cod !== 200) {
     data.weather[0].main === "Clouds" ? "☁️" :
     data.weather[0].main === "Clear" ? "☀️" : "🌤️";
 
+    const lat =data.coord.lat;
+    const lon = data.coord.lon;
+
+    map.setView([lat, lon], 10);
+
+    marker.setLatLng([lat, lon])
+          .bindPopuo(`${data.name} 🌤️`)
+          .openPopup();
+          
 } catch (error) {
     alert("City not found or API error");
 }
