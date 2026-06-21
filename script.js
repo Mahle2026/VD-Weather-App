624abd08e965a02315b2ebc5e2459c0973147e47
Vandice Tech <amahlephumla80@gmail.com>
2026-06-21 14:03:46 +0200

    commit

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ if (!city) {
 }
 
 const url = 
-`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid${apiKey}&units=metric`;
+`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 try {
 
